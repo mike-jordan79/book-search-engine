af1e52eb49da8da2fbaa84313fae078aba16e43e
mike-jordan79 <mike_jordan79@yahoo.com>
2022-11-10 20:02:56 -0500

    complete server.jsfile

--- solid-broccoli-main/Develop/server/server.js
+++ solid-broccoli-main/Develop/server/server.js
@@ -1,21 +1,52 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const db = require("./config/connection");
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const { typeDefs, resolvers } = require("./schema");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const routes = require("./routes");
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+async function startApolloServe(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+      return serverContext(req);
+    },
+  });
+
+  await server.start();
+
+  const app = express();
+  const PORT = process.env.PORT || 3001;
+
+  // app.use(express.urlencoded({ extended: true }));
+  // app.use(express.json());
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+
+  // app.use(routes);
+
+  server.applyMiddleware({
+    app,
+  });
+
+  await new Promise((resolve) => {
+    return app.listen(process.env.PORT || PORT, resolve);
+  });
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  // db.once("open", () => {
+  //   app.listen(PORT, () =>
+  //     console.log(`🌍 Now listening on localhost:${PORT}`)
+  //   );
+  // });
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApolloServe(typeDefs, resolvers);
